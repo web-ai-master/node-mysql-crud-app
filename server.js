069521eb624a6101de8810f563d49dd7015d8700
jovan0901 <jovanvelickovic0901@gmail.com>
2019-11-21 22:02:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 // define a root route
 app.get('/', (req, res) => {
-  res.send("Hello World");
+  res.send("Successfully Connected.");
 });
 
 // listen for requests
