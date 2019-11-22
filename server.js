75b25d91f85c44baa7751961a350d1568e996972
jovan0901 <jovanvelickovic0901@gmail.com>
2019-11-22 10:51:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const employeeRoutes = require('./src/routes/employee.routes')
 
 // using as middleware
 app.use('/api/v1/employees', employeeRoutes)
+
 // listen for requests
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
